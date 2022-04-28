e0cff6d2e9803530d779e9e4c48da33dc82ac6ac
정현모 <gusah009@naver.com>
2022-04-28 10:05:13 +0900

    assignment 1: read count 출력하는 user program 제작

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -14,6 +14,7 @@ struct {
 
 static struct proc *initproc;
 
+uint readcount = 0;
 int nextpid = 1;
 extern void forkret(void);
 extern void trapret(void);
@@ -531,4 +532,10 @@ procdump(void)
     }
     cprintf("\n");
   }
+}
+
+int
+getreadcount(void)
+{
+  return readcount;
 }
